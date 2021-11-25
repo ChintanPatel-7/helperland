27deaf66926abf60b012598dff9874e58b662866
ChintanPatel-7 <patelchintan7007@gmail.com>
2021-11-25 16:12:01 +0530

    Initial Commit -- Homepage

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("navbar-bg-color");
+        }
+        else{
+            $('.navbar').removeClass("navbar-bg-color");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+
+    $('.top-arrow').click(function(){
+        $('html').animate({scrollTop: 0})
+    });
+})
