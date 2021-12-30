c71c760aeb53e67b39d8e1c5195acc3c101194c1
ChintanPatel-7 <patelchintan7007@gmail.com>
2021-12-30 15:48:34 +0530

    Become-a-pro page compeleted

--- Html/assets/js/main.js
+++ Html/assets/js/main.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.homePage.navbar').addClass("navbar-bg-color");
+            $('.register-now-page.navbar').addClass("navbar-bg-color");
         }
         else{
             $('.homePage.navbar').removeClass("navbar-bg-color");
+            $('.register-now-page.navbar').removeClass("navbar-bg-color");
         }
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
