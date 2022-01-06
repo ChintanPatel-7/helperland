0d70bf4e16d8ea6740279f6e0e027dd467a9e605
ChintanPatel-7 <patelchintan7007@gmail.com>
2022-01-06 22:56:02 +0530

    1. reduce font size, padding for smaller device like mobile & tablet in all pages

--- Html/assets/js/main.js
+++ Html/assets/js/main.js
@@ -1,21 +1,79 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.homePage.navbar').addClass("navbar-bg-color");
             $('.register-now-page.navbar').addClass("navbar-bg-color");
         }
-        else{
+        else {
             $('.homePage.navbar').removeClass("navbar-bg-color");
             $('.register-now-page.navbar').removeClass("navbar-bg-color");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     })
 
-    $('.top-arrow').click(function(){
-        $('html').animate({scrollTop: 0})
+    //collapse(hide navbar) when scrolling
+    $(document).ready(function () {
+        $(window).scroll(function () {
+
+            $(".navbar .navbar-collapse").removeClass("show");
+            $('.backblack').removeClass("open");
+
+        });
     });
+
+    //black backgroung on open side navbar
+    $('.navbar-toggler').click(function () {
+        $('.backblack').addClass("open");
+    });
+    $('.backblack').click(function () {
+        $(".navbar .navbar-collapse").removeClass("show");
+        $('.backblack').removeClass("open");
+    });
+
+
+
+    // top Arrow
+    $('.top-arrow').click(function () {
+        $('html').animate({ scrollTop: 0 })
+    });
+
+    $("#btn-privacy-policy").click(function () {
+        document.getElementById("privacy-policy").style.setProperty('display', 'none', 'important')
+    });
+
+    $('#tbl-upcoming-service').DataTable({
+        paging: false,
+        bFilter: false,
+        ordering: true,
+        searching: false,
+        info: false,
+        "columnDefs": [
+            { "orderable": false, "targets": 4 }
+        ],
+        responsive: true,
+        "order": []
+    });
+
+    $('#tbl-service-history').DataTable({
+        paging: true,
+        "pagingType": "full_numbers",
+        // bFilter: false,
+        ordering: true,
+        searching: false,
+        info: true,
+        "columnDefs": [
+            { "orderable": false, "targets": 4 }
+        ],
+        "oLanguage": {
+            "sInfo": "Total Records: _TOTAL_"
+        },
+        "dom": '<"top">rt<"bottom"lip><"clear">',
+        responsive: true,
+        "order": []
+    });
+
 })
